ece3ac00c3dfc277240c49afe861c77a8a50a124
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2025-07-03 18:41:44 +0900

    シーン基底クラスの実装
    [BaseScene]
    ・シーン基底クラス
    ・ゲームオブジェクトの処理を回すことを前提とするため、初期化、更新、描画、終了処理はこのクラスメソッド内で共通処理化してメソッド継承を禁止する
    [ProjectFolderDesign.txt]
    ・シーン関連のクラスのフォルダ構成を追記

---
+++ DX11Framework-2025/src/Framework/Scenes/BaseScene.cpp
@@ -0,0 +1,53 @@
+﻿/**	@file	BaseScene.cpp
+*	@date	2025/07/03
+*/
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include"Framework/Scenes/BaseScene.h"
+
+//-----------------------------------------------------------------------------
+// RenderSystem Class
+//-----------------------------------------------------------------------------
+
+/// @brief	コンストラクタ
+BaseScene::BaseScene() {}
+
+/// @brief	デストラクタ
+BaseScene::~BaseScene(){}
+
+/**	@brief		ゲームオブジェクトの初期化処理を行う
+ *	@details	継承を禁止する
+ */
+void BaseScene::Initialize()
+{
+	// オブジェクトの生成
+	this->SetupObjects();
+}
+
+/**	@brief		オブジェクトの更新を行う
+ *	@param		float _deltaTime	デルタタイム
+ *	@details	継承を禁止する
+ */
+void BaseScene::Update(float _deltaTime)
+{
+
+}
+
+/**	@brief		ゲームオブジェクトの描画処理を行う
+ *	@param		float _deltaTime	デルタタイム
+ *	@details	継承を禁止する
+ */
+void BaseScene::Draw()
+{
+
+}
+
+/**	@brief		終了処理を行う
+ *	@details	継承を禁止する
+ */
+void BaseScene::Finalize()
+{
+
+}
---
+++ DX11Framework-2025/src/Framework/Scenes/BaseScene.h
@@ -0,0 +1,44 @@
+﻿/**	@file	BaseScene.h
+*	@date	2025/07/03
+*/
+#pragma once
+
+/**	@class		BaseScene
+ *	@brief		シーン基底クラス
+ */
+class BaseScene
+{
+protected:
+	/**	@brief		オブジェクトの生成、登録等を行う
+	 *	@details	純粋仮想関数
+	 */
+	virtual void SetupObjects() = 0;
+
+public:
+	/// @brief	コンストラクタ
+	BaseScene();
+	/// @brief	デストラクタ
+	virtual ~BaseScene();
+
+	/**	@brief		ゲームオブジェクトの初期化処理を行う
+	 *	@details	継承を禁止する
+	 */
+	virtual void Initialize()final;
+
+	/**	@brief		オブジェクトの更新を行う
+	 *	@param		float _deltaTime	デルタタイム
+	 *	@details	継承を禁止する
+	 */
+	virtual void Update(float _deltaTime)final;
+
+	/**	@brief		ゲームオブジェクトの描画処理を行う
+	 *	@param		float _deltaTime	デルタタイム
+	 *	@details	継承を禁止する
+	 */
+	virtual void Draw()final;
+
+	/**	@brief		終了処理を行う
+	 *	@details	継承を禁止する
+	 */
+	virtual void Finalize()final;
+};

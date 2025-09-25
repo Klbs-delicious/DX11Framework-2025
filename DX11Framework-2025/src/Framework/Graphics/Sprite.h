f68426f36c4b058fa394e00386a5f8f1b737b2e8
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2025-09-25 11:41:44 +0900

    画像の管理クラスの実装
    [Sprite.h]
    ・画像情報を保持する構造体
    [IResourceManager.h]
    ・リソース管理インターフェース
    [SpriteManager]
    ・画像の生成、保持破棄を管理する
    ・ファイルパスは日本語文字列にも対応できるようにstd::u8stringを使用
    ・現在は一括で画像名とそれに対応するファイルパスを登録する形にしている
    →生成はシーンで必要なものだけをその時に読み込む想定

---
+++ DX11Framework-2025/src/Framework/Graphics/Sprite.h
@@ -0,0 +1,22 @@
+﻿/**	@file	Sprite.h
+*	@date	2025/09/24
+*/
+#pragma once
+#include"Framework/Utils/CommonTypes.h"
+#include <d3d11.h>
+
+/**	@brief スプライト情報
+ */
+struct Sprite
+{
+	DX::ComPtr<ID3D11ShaderResourceView> texture;	///< テクスチャ情報
+	int width;										///< テクスチャの幅
+	int height;										///< テクスチャの高さ
+	int bpp;										///< テクスチャの1ピクセルあたりのビット数
+
+	Sprite() :texture(nullptr), width(0), height(0), bpp(0) {}
+
+	~Sprite() {
+		if (texture) { texture.Reset(); }
+	}
+};

61a16c109c3663231a69da99c7dab33a028e5d82
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2026-02-05 16:09:18 +0900

    [変更]  ShaderBase の読み込み・コンパイル経路改善
    ## 概要
    - シェーダー読み込み処理を見直して.csoファイルの生成階層などを整理した
    ## 変更内容
    [ShaderBase]
    - hlsl / cso のパス生成処理を明確化した
      - Debug ビルドでは常に hlsl をコンパイルし、.cso を生成する挙動に変更した
      - Release ビルドでは .cso を優先して読み込み、失敗時はフォールバックで再コンパイルするよう修正した
    - シェーダーの読み込み・コンパイル失敗時に、詳細なエラーメッセージを表示する処理を追加した

--- DX11Framework-2025/Code/Include/Framework/Shaders/ShaderBase.h
+++ DX11Framework-2025/Code/Include/Framework/Shaders/ShaderBase.h
@@ -7,6 +7,7 @@
 
 #include<string>
 #include<array>
+#include <filesystem>
 
 #include <d3d11.h>
 #pragma comment(lib,"d3d11.lib")
@@ -49,16 +50,40 @@ protected:
 	 *	@param const ShaderInfo _shaderInfo シェーダー情報
 	 *	@return ComPtr<ID3DBlob>            シェーダーバイナリ
 	 */
-	void LoadShader(ID3D11Device& _device, const ShaderInfo _shaderInfo)const;
+	void LoadShader(ID3D11Device& _device, const ShaderInfo _shaderInfo);
 
 	mutable DX::ComPtr<ID3DBlob> blob;	///< シェーダーバイナリ
 
 private:
 	/** @brief シェーダーのコンパイル
-	 *  @param ID3D11Device& _device D3D11のデバイス
-	 *  @param ShaderInfo _shaderInfo シェーダー情報
-	 *  @return bool コンパイルに成功したら true
+	 *  @param _device     D3D11のデバイス
+	 *  @param _shaderInfo シェーダー情報
+	 *  @param _saveCso    .cso を書き出す場合 true
+	 *  @return コンパイルに成功したら true
 	 */
-	bool CompileShader(ID3D11Device& _device, const ShaderInfo _shaderInfo)const;
+	bool CompileShader(ID3D11Device& _device, const ShaderInfo _shaderInfo, bool _saveCso);
 
+	/** @brief シェーダーの種類からディレクトリ名を取得
+	 *  @param ShaderType _type シェーダーの種類
+	 *  @return std::wstring ディレクトリ名
+	 */
+	static std::wstring ShaderTypeToDirectory(ShaderType _type);
+
+	/**	@brief	シェーダーファイルのパスを取得
+	 *	@param const ShaderInfo _shaderInfo シェーダー情報
+	 *	@return std::wstring シェーダーファイルのパス
+	 */
+	static std::wstring MakeCsoPath(const ShaderInfo& _shaderInfo);
+
+	/**	@brief	シェーダーファイルのパスを取得
+	 *	@param const ShaderInfo _shaderInfo シェーダー情報
+	 *	@return std::wstring シェーダーファイルのパス
+	 */
+	std::wstring MakeHlslPath(const ShaderInfo& _shaderInfo)const ;
+
+	/**	@brief	シェーダーファイルのパスを取得
+	 *	@param const ShaderInfo _shaderInfo シェーダー情報
+	 *	@return std::wstring シェーダーファイルのパス
+	 */
+	static void EnsureDirectoryForFile(const std::wstring& _filePath);
 };
--- DX11Framework-2025/Code/Source/Framework/Shaders/ShaderBase.cpp
+++ DX11Framework-2025/Code/Source/Framework/Shaders/ShaderBase.cpp
@@ -1,23 +1,81 @@
 ﻿/** @file   ShaderBase.cpp
-*   @date   2025/10/05
-*/
+ *  @date   2025/10/05
+ */
 
-//-----------------------------------------------------------------------------
-// Includes
-//-----------------------------------------------------------------------------
+ //-----------------------------------------------------------------------------
+ // Includes
+ //-----------------------------------------------------------------------------
 #include "Include/Framework/Shaders/ShaderBase.h"
 #include "Include/Framework/Shaders/ShaderCommon.h"
 
 #include <d3dcompiler.h>
 #pragma comment(lib, "d3dcompiler.lib")
 
+#include <Windows.h>
+
+#include <filesystem>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
 using namespace ShaderCommon;
+
+//-----------------------------------------------------------------------------
+// Local Helpers
+//-----------------------------------------------------------------------------
+namespace
+{
+	/** @brief HRESULT を人が読める文字列に変換する
+	 *  @param _hr HRESULT
+	 *  @return std::wstring メッセージ
+	 */
+	static std::wstring HrToMessageW(HRESULT _hr)
+	{
+		LPWSTR buffer = nullptr;
+
+		const DWORD flags =
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS;
+
+		const DWORD size = FormatMessageW(
+			flags,
+			nullptr,
+			static_cast<DWORD>(_hr),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			reinterpret_cast<LPWSTR>(&buffer),
+			0,
+			nullptr);
+
+		std::wstring msg;
+		if (size != 0 && buffer)
+		{
+			msg.assign(buffer, buffer + size);
+			LocalFree(buffer);
+		}
+		else
+		{
+			msg = L"(エラーメッセージ取得失敗)";
+		}
+
+		return msg;
+	}
+
+	/** @brief 実行時にユーザーへエラーを通知する
+	 *  @param _title タイトル
+	 *  @param _body  本文
+	 */
+	static void ShowErrorPopupW(const wchar_t* _title, const std::wstring& _body)
+	{
+		MessageBoxW(nullptr, _body.c_str(), _title, MB_OK | MB_ICONERROR);
+	}
+}
 
 //-----------------------------------------------------------------------------
 // ShaderBase class
 //-----------------------------------------------------------------------------
 
-ShaderBase::ShaderBase() :blob(nullptr) {}
+ShaderBase::ShaderBase() : blob(nullptr) {}
 
 ShaderBase::~ShaderBase()
 {
@@ -25,65 +83,166 @@ ShaderBase::~ShaderBase()
 }
 
 /**	@brief	シェーダーファイルからバイナリデータを読み込む
- *	@param ID3D11Device&    _device	    D3D11のデバイス
- *	@param const ShaderInfo _shaderInfo シェーダー情報
- *	@return ComPtr<ID3DBlob>            シェーダーバイナリ
+ *	@param	_device	    D3D11のデバイス
+ *	@param	_shaderInfo シェーダー情報
  */
-void ShaderBase::LoadShader(ID3D11Device& _device, const ShaderInfo _shaderInfo)const
+void ShaderBase::LoadShader(ID3D11Device& _device, const ShaderInfo _shaderInfo)
 {
 #ifdef _DEBUG
-    this->CompileShader(_device, _shaderInfo);
-
+	// Debug は常に hlsl をコンパイルして最新を使い、.cso を書き出す
+	this->CompileShader(_device, _shaderInfo, true);
 #else
-    // 指定のシェーダーファイルを読み込み
-    std::wstring fullPath = L"Assets/Shaders/" + _shaderInfo.filePath + L".cso";
+	// Release は .cso を読み込む（無い場合はフォールバックでコンパイルして .cso を生成する）
+	const std::wstring csoPath = ShaderBase::MakeCsoPath(_shaderInfo);
 
-    HRESULT hr = D3DReadFileToBlob(fullPath.c_str(), this->blob.GetAddressOf());
+	const HRESULT hr = D3DReadFileToBlob(csoPath.c_str(), this->blob.GetAddressOf());
+	if (FAILED(hr))
+	{
+		std::wstringstream ss;
+		ss << L".cso の読み込みに失敗しました。\n\n"
+			<< L"Path:\n" << csoPath << L"\n\n"
+			<< L"HRESULT: 0x"
+			<< std::hex << std::setw(8) << std::setfill(L'0') << hr << L"\n"
+			<< L"Message:\n" << HrToMessageW(hr);
 
-    if (FAILED(hr)) {
-        OutputDebugString(L"[CSO Load Error] シェーダーバイナリの読み込みに失敗しました。\n");
+		ShowErrorPopupW(L"Shader Load Error", ss.str());
+		OutputDebugString(L"[CSO Load Error] シェーダーバイナリの読み込みに失敗しました。\n");
 
-        // 読み込みに失敗した場合はコンパイルする
-        this->CompileShader(_device, _shaderInfo);
-        return;
-    }
+		// フォールバックとして hlsl からコンパイルし、次回以降は .cso 読み込みで済むようにする
+		this->CompileShader(_device, _shaderInfo, true);
+		return;
+	}
 #endif
 }
 
 /** @brief シェーダーのコンパイル
- *  @param ID3D11Device& _device D3D11のデバイス
- *  @param ShaderInfo _shaderInfo シェーダー情報
- *  @return bool コンパイルに成功したら true
+ *  @param _device     D3D11のデバイス
+ *  @param _shaderInfo シェーダー情報
+ *  @param _saveCso    .cso を書き出す場合 true
+ *  @return コンパイルに成功したら true
  */
-bool ShaderBase::CompileShader(ID3D11Device& _device, const ShaderInfo _shaderInfo)const
+bool ShaderBase::CompileShader(ID3D11Device& _device, const ShaderInfo _shaderInfo, bool _saveCso) 
 {
-    DX::ComPtr<ID3DBlob> errorBlob;
+	DX::ComPtr<ID3DBlob> errorBlob;
 
-    // 指定のシェーダーファイルをコンパイル
-    std::wstring fullPath = L"Code/Shaders/" + _shaderInfo.filePath + L".hlsl";
+	const std::wstring hlslPath = ShaderBase::MakeHlslPath(_shaderInfo);
 
-    HRESULT hrVS = D3DCompileFromFile(
-        fullPath.c_str(),
-        nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE,
-        EntryPointName[static_cast<int>(_shaderInfo.shaderType)].c_str(), TargetName[static_cast<int>(_shaderInfo.shaderType)].c_str(), 0, 0,
-        this->blob.GetAddressOf(), errorBlob.GetAddressOf());
+	const HRESULT hr = D3DCompileFromFile(
+		hlslPath.c_str(),
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		EntryPointName[static_cast<int>(_shaderInfo.shaderType)].c_str(),
+		TargetName[static_cast<int>(_shaderInfo.shaderType)].c_str(),
+		0,
+		0,
+		this->blob.GetAddressOf(),
+		errorBlob.GetAddressOf());
 
-    if(SUCCEEDED(hrVS))
-    {
-        return true;
+	if (SUCCEEDED(hr))
+	{
+		if (_saveCso)
+		{
+			// コンパイル成功時に .cso を書き出す（Debug 用キャッシュ、Release フォールバック時の自己修復）
+			const std::wstring csoPath = ShaderBase::MakeCsoPath(_shaderInfo);
+
+			// 親ディレクトリが無いと書き込みに失敗するため、事前に作成する
+			ShaderBase::EnsureDirectoryForFile(csoPath);
+
+			const HRESULT hrWrite = D3DWriteBlobToFile(this->blob.Get(), csoPath.c_str(), TRUE);
+			if (FAILED(hrWrite))
+			{
+				std::wstringstream ss;
+				ss << L".cso の書き出しに失敗しました。\n\n"
+					<< L"Path:\n" << csoPath << L"\n\n"
+					<< L"HRESULT: 0x"
+					<< std::hex << std::setw(8) << std::setfill(L'0') << hrWrite << L"\n"
+					<< L"Message:\n" << HrToMessageW(hrWrite);
+
+				ShowErrorPopupW(L"Shader Save Error", ss.str());
+				OutputDebugString(L"[CSO Save Error] シェーダーバイナリの書き出しに失敗しました。\n");
+			}
+		}
+
+		return true;
 	}
 
-    if (errorBlob)
-    {
-        const char* errorMsg = static_cast<const char*>(errorBlob->GetBufferPointer());
-        std::string errorString(errorMsg, errorBlob->GetBufferSize());
-        std::wstring wErrorString(errorString.begin(), errorString.end());
-        OutputDebugString(L"[Shader Compilation Error]\n");
-        OutputDebugString(wErrorString.c_str());
-    }
-    else
-    {
-        OutputDebugString(L"シェーダーのコンパイルに失敗しましたが、errorBlob に情報がありません。\n");
-    }
-    return false;
+	if (errorBlob)
+	{
+		const char* errorMsg = static_cast<const char*>(errorBlob->GetBufferPointer());
+		std::string errorString(errorMsg, errorBlob->GetBufferSize());
+		std::wstring wErrorString(errorString.begin(), errorString.end());
+
+		OutputDebugString(L"[Shader Compilation Error]\n");
+		OutputDebugString(wErrorString.c_str());
+
+#ifndef _DEBUG
+		std::wstringstream ss;
+		ss << L"シェーダーのコンパイルに失敗しました。\n\n"
+			<< L"Path:\n" << hlslPath << L"\n\n"
+			<< L"Message:\n" << wErrorString;
+
+		ShowErrorPopupW(L"Shader Compile Error", ss.str());
+#endif
+	}
+	else
+	{
+		OutputDebugString(L"シェーダーのコンパイルに失敗しましたが、errorBlob に情報がありません。\n");
+
+#ifndef _DEBUG
+		std::wstringstream ss;
+		ss << L"シェーダーのコンパイルに失敗しました。\n\n"
+			<< L"Path:\n" << hlslPath << L"\n\n"
+			<< L"詳細情報が取得できませんでした。";
+
+		ShowErrorPopupW(L"Shader Compile Error", ss.str());
+#endif
+	}
+
+	return false;
+}
+
+/** @brief シェーダーの種類からディレクトリ名を取得
+ *  @param _type シェーダーの種類
+ *  @return ディレクトリ名
+ */
+std::wstring ShaderBase::ShaderTypeToDirectory(ShaderType _type)
+{
+	switch (_type)
+	{
+	case ShaderType::VertexShader:
+		return L"VertexShader";
+	case ShaderType::PixelShader:
+		return L"PixelShader";
+	case ShaderType::GeometryShader:
+		return L"GeometryShader";
+	default:
+		return L"UnknownShader";
+	}
+}
+
+/** @brief シェーダーファイルのパスを取得
+ *  @param _shaderInfo シェーダー情報
+ *  @return シェーダーファイルのパス
+ */
+std::wstring ShaderBase::MakeCsoPath(const ShaderInfo& _shaderInfo)
+{
+	return L"Assets/Shaders/" + _shaderInfo.filePath + L".cso";
+}
+
+/** @brief シェーダーファイルのパスを取得
+ *  @param _shaderInfo シェーダー情報
+ *  @return シェーダーファイルのパス
+ */
+std::wstring ShaderBase::MakeHlslPath(const ShaderInfo& _shaderInfo) const
+{
+	return L"Code/Shaders/" + _shaderInfo.filePath + L".hlsl";
+}
+
+/** @brief 指定されたファイルパスの親ディレクトリを保証する
+ *  @param _filePath ファイルパス
+ */
+void ShaderBase::EnsureDirectoryForFile(const std::wstring& _filePath)
+{
+	std::filesystem::path p(_filePath);
+	std::filesystem::create_directories(p.parent_path());
 }

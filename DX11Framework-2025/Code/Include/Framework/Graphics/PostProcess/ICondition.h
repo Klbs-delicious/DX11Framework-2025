038c08c025e6ac5fd676c0a25053c9a0288b7808
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2026-04-10 08:06:31 +0900

    [追加] 特定のデータ、状態などを評価して条件結果を返すインターフェースを作成した
    ## 概要
    ポスト処理を行えるかどうかを判定する際に、条件を監視するコンポーネント側に注入しやすい形になるよう意識して作成した。
    
    ## 変更内容
    - 特定のデータ、状態などを評価して条件結果を返すインターフェース ICondition を作成

---
+++ DX11Framework-2025/Code/Include/Framework/Graphics/PostProcess/ICondition.h
@@ -0,0 +1,18 @@
+/**	@file	ICondition.h
+*	@date	2026/04/10
+*/
+#pragma once
+
+/**	@class		ICondition
+ *	@brief		特定のデータ、状態などを評価して条件結果を返すインターフェース
+ */
+class ICondition
+{
+public:
+	virtual ~ICondition() = default;
+
+	/**	@brief	条件を評価する
+	 *	@return	bool 条件が満たされている場合は true、そうでない場合は false
+	 */
+	virtual bool Check() = 0;
+};
